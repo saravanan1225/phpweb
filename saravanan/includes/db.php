f3edd4b0ddb9d70026a17ad5921e6470423291d1
saravanan1225 <60806369+saravanan1225@users.noreply.github.com>
2020-11-19 11:27:07 +0530

    Update db.php

--- saravanan/includes/db.php
+++ saravanan/includes/db.php
@@ -1,5 +1,5 @@
 <?php 
 
-$con = mysqli_connect("4vXngzVbTQ","sql12377494","4vXngzVbTQ","ecom_store");
+$con = mysqli_connect("sql12.freemysqlhosting.net","sql12377494","4vXngzVbTQ","ecom_store");
 
 ?>

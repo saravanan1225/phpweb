13d9e4bb6700407ab541d03652394f496654a088
saravanan1225 <60806369+saravanan1225@users.noreply.github.com>
2020-12-03 13:07:12 +0530

    Update db.php

--- saravanan/includes/db.php
+++ saravanan/includes/db.php
@@ -1,5 +1,5 @@
 <?php 
 
-$con = mysqli_connect("http://sql12.freemysqlhosting.net","sql12379967","rllLhWbVSN","sql12379967");
+$con = mysqli_connect("sql12.freemysqlhosting.net","sql12379967","rllLhWbVSN","sql12379967");
 
 ?>
